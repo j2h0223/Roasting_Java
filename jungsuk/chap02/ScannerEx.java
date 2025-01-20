8609507fbb5540f2cffee4199c10509eed2942a1
R2B <85736180+j2h0223@users.noreply.github.com>
2025-01-20 19:21:04 +0900

    변경

--- jungsuk/chap02/ScannerEx.java
+++ jungsuk/chap02/ScannerEx.java
@@ -9,6 +9,7 @@ public class ScannerEx {
         String input = scanner.nextLine();
         int num = Integer.parseInt(input); // 입력받은 문자열을 숫자로 변환
 
+
         System.out.println("입력내용 : " + input);
         System.out.printf("num = %d%n", num);
     }
