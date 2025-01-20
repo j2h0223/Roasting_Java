0d6896caad936e7c9a36096b312244ebb9cfb8ea
R2B <85736180+j2h0223@users.noreply.github.com>
2025-01-20 19:22:20 +0900

    변경

---
+++ jungsuk/chap03/OperatorEx25.java
@@ -0,0 +1,18 @@
+package jungsuk.chap03;
+import java.util.*;
+
+public class OperatorEx25 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        char ch;
+
+        String input = scanner.nextLine();
+        ch = input.charAt(0);
+
+        if ('0'<= ch && ch <= '9')
+            System.out.printf("입력하신 문자는 숫자입니다.%n");
+
+        if (('a'<= ch && ch <= 'z') || ('A'<= ch && ch <= 'Z'))
+            System.out.printf("입력하신 문자는 영문자입니다.%n");
+    }
+}
