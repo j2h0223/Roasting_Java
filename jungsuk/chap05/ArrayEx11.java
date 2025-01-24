1f1cb690c1af737f370f84cd01d04d799cf9e5ca
R2B <85736180+j2h0223@users.noreply.github.com>
2025-01-24 17:04:26 +0900

    변경

---
+++ jungsuk/chap05/ArrayEx11.java
@@ -0,0 +1,24 @@
+package jungsuk.chap05;
+
+public class ArrayEx11 {
+    public static void main(String[] args){
+        int[] numArr = new int[10];
+        int[] counter = new int[10];
+
+        for (int i=0; i < numArr.length; i++){
+//            numArr[i] = (int)(Math.random() * 10);
+//            System.out.print(numArr[i]);
+            System.out.print(numArr[i] = (int)(Math.random()*10));
+        }
+        System.out.println();
+
+//        for (int i = 0; i < numArr.length; i++){
+        for(int i : numArr){
+            counter[numArr[i]]++;
+        }
+
+        for (int i =0; i < numArr.length; i++){
+            System.out.println(i + "의 개수 : " + counter[i]);
+        }
+    } // end main
+}
