1fdf8801e2f4d9358cce36f6b127aaf7060d6263
R2B <85736180+j2h0223@users.noreply.github.com>
2025-02-07 19:39:15 +0900

    변경

---
+++ jungsuk/chap08/ExceptionEx14.java
@@ -0,0 +1,16 @@
+package jungsuk.chap08;
+
+class ExceptionEx14 {
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception e) {
+            System.out.println("main 메서드에서 예외 처리");
+            e.printStackTrace();
+        }
+    }
+
+    static void method1() throws Exception {
+        throw new Exception();
+    }
+}
