537e153870f6845b7d88e24758731b3ee6c1aab0
R2B <85736180+j2h0223@users.noreply.github.com>
2025-02-07 16:58:04 +0900

    변경

---
+++ jungsuk/chap07/InnerEx6.java
@@ -0,0 +1,21 @@
+package jungsuk.chap07;
+
+class InnerEx6 {
+    Object iv = new Object(){
+        void method(){
+
+        }
+    };
+    static Object cv = new Object(){
+        void method(){
+
+        }
+    };
+    void myMethod(){
+        Object lv = new Object(){
+            void method(){
+
+            }
+        };
+    }
+}
